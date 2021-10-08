f4182242a8dff85fc4e9ca12f96633875705373b
Jesse Lentz <jnlentz@gmail.com>
2021-10-08 00:31:48 -0700

    aSDF

--- trees.py
+++ trees.py
@@ -1,3 +1,4 @@
+import random
 class tree_node:
     def __init__(self, val):
         self.val = val
@@ -5,32 +6,45 @@ class tree_node:
         self.left = None
 
 def insert(root, val):
-    if root == None: return
-    if root.val == val: return
-    if val < root.val:
-        if root.left != None:
-            insert(root.left, val)
-        else:
-            root.left = tree_node(val)
-    elif val > root.val:
-        if root.right != None:
-            insert(root.right, val)
-        else:
-            root.right = tree_node(val)
-            
-           
+    if root is None: return tree_node(val)
+    if root.val == val:
+        return root
+    if root.val > val:
+        root.left = insert(root.left, val)
+    elif root.val < val:
+        root.right = insert(root.right, val)
+    return root
     
-
 def print_tree(root):
     if root == None: return
     print_tree(root.left)
     print(root.val)
     print_tree(root.right)
+
+def height(root):
+    if root is None:
+        return 1
+    else:
+        return 1+max(height(root.right),height(root.left))
+
+def numLeaves(root):
+    if not root:
+        return 0
+    if not root.left and not root.right:
+        return 1
+    else:
+        return numLeaves(root.right)+numLeaves(root.left)
+            
+           
     
 
-vals = [3,9,2,34,76, 23, 93]
+
+    
+nums = range(100)
 root = tree_node(50)
-for i in vals:
-    insert(root, i)
+for i in range(9):
+    val = random.choice(nums)
+    insert(root, val)
 
 print_tree(root)
+print('s = ', numLeaves(root))
